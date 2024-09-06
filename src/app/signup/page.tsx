785f4d42e2631db6f4ec850c1c3d65fd16918701
Amit Biswas <amit.142biswas@gmail.com>
2024-09-06 21:32:35 +0530

    log

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,37 +1,41 @@
 "use client";
-
-import { useEffect, useState } from "react";
+import Link from "next/link";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import axios from "axios";
 import { toast } from "react-hot-toast";
-import { useRouter } from "next/navigation";
-import Link from "next/link";
 
 export default function SignupPage() {
   const router = useRouter();
-
-  const [user, setUser] = useState({
+  const [user, setUser] = React.useState({
     email: "",
     password: "",
     username: "",
   });
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
 
-  const [buttonDisabled, setButtonDisabled] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  const onSignUp = async () => {
+  const onSignup = async () => {
     try {
       setLoading(true);
-      const response = await axios.post("/api/user/signup", user);
-      console.log("Signup sucess", response.data);
+      const response = await axios.post("/api/users/signup", user);
+      console.log("Signup success", response.data);
       router.push("/login");
     } catch (error: any) {
-      console.log("Signup Failed");
+      console.log("Signup failed", error.message);
+
       toast.error(error.message);
+    } finally {
+      setLoading(false);
     }
   };
 
   useEffect(() => {
-    if (user.email.length > 0 && user.password.length > 0) {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.username.length > 0
+    ) {
       setButtonDisabled(false);
     } else {
       setButtonDisabled(true);
@@ -46,31 +50,31 @@ export default function SignupPage() {
       <input
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
         id="username"
+        type="text"
         value={user.username}
         onChange={(e) => setUser({ ...user, username: e.target.value })}
         placeholder="username"
-        type="text"
       />
       <label htmlFor="email">email</label>
       <input
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
         id="email"
+        type="text"
         value={user.email}
         onChange={(e) => setUser({ ...user, email: e.target.value })}
         placeholder="email"
-        type="text"
       />
       <label htmlFor="password">password</label>
       <input
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
         id="password"
+        type="password"
         value={user.password}
         onChange={(e) => setUser({ ...user, password: e.target.value })}
-        type="text"
         placeholder="password"
       />
       <button
-        onClick={onSignUp}
+        onClick={onSignup}
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
       >
         {buttonDisabled ? "No signup" : "Signup"}
--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -1,6 +1,22 @@
-import React from 'react'
+'use client'
 
-export default function page() {
+import axios from 'axios'
+import React, {useState} from 'react'
+
+export default function VerifyEmailPage() {
+  const [token, setToken] = useState("")
+  const [verified, setVerified] = useState(false)
+  const [error, setError] = useState(false)
+
+  const verifyUserEmail = async() => {
+    try {
+      await axios.post("/api/users/verifyemail", {token})
+      setVerified(true)
+    } catch (error:any) {
+      setError(true)
+    }
+  }
+
   return (
     <div>page</div>
   )
